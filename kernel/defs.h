52076007bea4655a6a920d3adabe57cf2e98d668
Yi-Dan212 <864767916@qq.com>
2024-05-15 18:04:02 +0800

    cow

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,7 +63,10 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-
+int             kaddrefcnt(void* pa);
+int             krefcnt(void* pa);
+void*           cowalloc(pagetable_t pagetable, uint64 va);
+int             cowpage(pagetable_t pagetable, uint64 va);
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -171,7 +174,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-
+pte_t *         walk(pagetable_t pagetable, uint64 va, int alloc);
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
